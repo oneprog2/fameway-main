dba2fd08ba1d722f707ba3b84d24c3c670bdf0e6
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-08 02:30:01 +0100

    Login data

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,28 +1,30 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import { configureStore } from './redux/Store';
-import * as serviceWorker from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
-import Spinner from './views/spinner/Spinner';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { configureStore } from "./redux/Store";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+import Spinner from "./views/spinner/Spinner";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <Suspense fallback={<Spinner />}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Suspense>
-  </Provider>,
-  document.getElementById('root'),
+  <Auth0Provider
+    domain="dev-lzkir111o3ip75a0.us.auth0.com"
+    clientId="fvFanDY5XxF2Op7kITivNKSox5mv6e6M"
+    redirectUri={window.location.origin}
+  >
+    <Provider store={configureStore()}>
+      <Suspense fallback={<Spinner />}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Suspense>
+    </Provider>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
-// If you want to enable client cache, register instead.
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- admin/src/views/dashboard/Dashboard.js
+++ admin/src/views/dashboard/Dashboard.js
@@ -1,50 +1,71 @@
-import React from 'react';
-import { Grid, Button, Box, Card, CardContent, Typography } from '@mui/material';
-import FeatherIcon from 'feather-icons-react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import {
+  Grid,
+  Button,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import FeatherIcon from "feather-icons-react";
+import { NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import ThemeSelect from './ThemeSelect';
+import ThemeSelect from "./ThemeSelect";
 
-import Breadcrumb from '../../layouts/full-layout/breadcrumb/Breadcrumb';
-import PageContainer from '../../components/container/PageContainer';
+import Breadcrumb from "../../layouts/full-layout/breadcrumb/Breadcrumb";
+import PageContainer from "../../components/container/PageContainer";
 
-const Dashboard1 = () => (
-  // 2
+const Dashboard1 = () => {
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0();
 
-  <PageContainer title="Starter Page" description="this is Starter Page">
-    {/* breadcrumb */}
-    <Breadcrumb title="Starter Page" subtitle="Welcome Johnathan">
-      <ThemeSelect />
-      <Button
-        to="/user-profile"
-        component={NavLink}
-        sx={{
-          ml: 1,
-        }}
-        variant="contained"
-        color="primary"
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  if (!isAuthenticated) return null;
+
+  return (
+    <PageContainer title="Starter Page" description="this is Starter Page">
+      {/* breadcrumb */}
+      <Breadcrumb
+        title={`Hello ${user?.name}`}
+        subtitle={`C'est le come back !`}
       >
-        <FeatherIcon icon="user" width="18" height="18" />
-        <Box fontWeight="400" sx={{ ml: 1 }}>
-          Profile
-        </Box>
-      </Button>
-    </Breadcrumb>
-    {/* end breadcrumb */}
-    <Grid container spacing={0}>
-      {/* ------------------------- row 1 ------------------------- */}
-      <Grid item xs={12} lg={12}>
-        <Card>
-          <CardContent>
-            <Typography variant="h4">Starter Card</Typography>
-            <Typography variant="body1">
-              This impressive paella is a perfect party dish and a fun meal to cook together with
-              your guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-          </CardContent>
-        </Card>
+        <ThemeSelect />
+        <Button
+          to="/user-profile"
+          component={NavLink}
+          sx={{
+            ml: 1,
+          }}
+          variant="contained"
+          color="primary"
+        >
+          <FeatherIcon icon="user" width="18" height="18" />
+          <Box fontWeight="400" sx={{ ml: 1 }}>
+            Profile
+          </Box>
+        </Button>
+      </Breadcrumb>
+      {/* end breadcrumb */}
+      <Grid container spacing={0}>
+        {/* ------------------------- row 1 ------------------------- */}
+        <Grid item xs={12} lg={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h4">Starter Card</Typography>
+              <Typography variant="body1">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
-  </PageContainer>
-);
+    </PageContainer>
+  );
+};
+
 export default Dashboard1;
