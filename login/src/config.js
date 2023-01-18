2e1abd317f2b8ddee4356a1864251ef3fe85c2aa
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-18 18:36:33 +0100

    config login

--- login/src/config.js
+++ login/src/config.js
@@ -1,4 +1,4 @@
-export const AUTH0_DOMAIN = "dev-lzkir111o3ip75a0.us.auth0.com";
+export const AUTH0_DOMAIN = "auth.fameway.co";
 export const AUTH0_CLIENT_ID = "fvFanDY5XxF2Op7kITivNKSox5mv6e6M";
 export const AUTH0_LOGIN_REDIRECT_URI = "admin.fameway.co";
 export const AUTH0_REALM = "Username-Password-Authentication";
