e17709cb88ee9232ab479235d433a76716bec043
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-19 00:24:41 +0100

    better

--- login/src/config.js
+++ login/src/config.js
@@ -1,6 +1,6 @@
 export const AUTH0_DOMAIN = "auth.fameway.co";
 export const AUTH0_CLIENT_ID = "fvFanDY5XxF2Op7kITivNKSox5mv6e6M";
-export const AUTH0_LOGIN_REDIRECT_URI = "http://admin.fameway.co";
+export const AUTH0_LOGIN_REDIRECT_URI = "https://admin.fameway.co";
 export const AUTH0_REALM = "Username-Password-Authentication";
 export const AUTH0_USER_SCOPE = "openid email profile";
 export const LOGIN_RESPONSE_TYPE = "token id_token code";
