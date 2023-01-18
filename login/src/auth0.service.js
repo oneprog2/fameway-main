889b9daa058a652bf5d4770be478f516513ed4a5
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-19 00:35:58 +0100

    another

--- login/src/auth0.service.js
+++ login/src/auth0.service.js
@@ -8,6 +8,10 @@ import {
 } from "./config";
 
 export const auth = new auth0.WebAuth({
+  overrides: {
+    __tenant: "dev-lzkir111o3ip75a0",
+    __token_issuer: "auth.fameway.co",
+  },
   scope: AUTH0_USER_SCOPE,
   domain: AUTH0_DOMAIN,
   clientID: AUTH0_CLIENT_ID,
