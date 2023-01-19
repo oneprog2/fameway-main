02aa1241115b94ac9350eae6e7aa589d608aaf78
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-19 11:45:03 +0100

    again

--- admin/src/layouts/full-layout/footer/Footer.js
+++ admin/src/layouts/full-layout/footer/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const Footer = () => (
-  <Box sx={{ p: 3, textAlign: 'center' }}>
-    <Typography>© 2023 Tout droits reservés - Fameway <3 </Typography>
+  <Box sx={{ p: 3, textAlign: "center" }}>
+    <Typography>© 2023 Tout droits reservés - Fameway</Typography>
   </Box>
 );
 
