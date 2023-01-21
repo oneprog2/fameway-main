ddd0d5375d11fc90c80c11e06ea0c837de86c24b
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-21 22:43:05 +0100

    dz

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,14 +15,15 @@ app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  res.send(`Hello, time! The time from the DB is ${rows[0].now}`);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.post("/create_user_event", async (req, res) => {
   console.log(req.body);
   res.send(`Hello, World`);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
