8580493741e977bd203071b05a321fabb0075276
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-21 22:44:17 +0100

    tete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,12 @@
-import bodyParser from "body-parser";
 import express from "express";
-import pg from "pg";
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, time! The time from the DB is ${rows[0].now}`);
+  res.send(`Hello, time! The time from the DB is no`);
 });
 
 const server = app.listen(port, () => {
